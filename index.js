ea1beeb262d237e057f089a388b935d17ab9b717
imjeremiah <52246955+imjeremiah@users.noreply.github.com>
2024-05-17 13:14:55 -0700

    Add computer choice logic

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Hello World");
+function getComputerChoice() {
+    let choice = "";
+
+    let rand = Math.random();
+
+    if (rand <= .33) {
+        choice = "rock";
+    } else if (rand >= .66) {
+        choice = "paper";
+    } else {
+        choice = "scissors"
+    }
+
+    return choice;
+}
+
+console.log(getComputerChoice());
